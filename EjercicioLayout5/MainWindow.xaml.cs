b9bee6074679b8864c4a4ee1799ac8133e681b25
Pedro <citadel153@hotmail.com>
2020-10-26 11:57:50 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioLayout5/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjercicioLayout5
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumeroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button boton = (Button)sender;
+
+
+            OperacionTextBlock.Text += boton.Content.ToString();
+
+
+        }
+    }
+}
